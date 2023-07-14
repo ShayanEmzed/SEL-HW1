1615b79cdba148d2e17e5fea80b03ba141f33dfd
ShayanEmzed <shayan80sama@gmail.com>
2023-07-14 16:37:50 +0330

    Randomize puncuation choice
    Old version always cosidered first one-third of puncuation, changed it to randomize process.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import random
 
 letters = string.ascii_letters
 digits = string.digits
-special_chars = string.punctuation[:len(string.punctuation) // 3]
+special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, 50)]
 
-pwd_len = 6
+pwd_len = 16
 alphabet = letters + digits + special_chars
 
 pwd = ''
