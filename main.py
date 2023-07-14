add60a60460d2aa87d61979b39777e8d35962870
ShayanEmzed <shayan80sama@gmail.com>
2023-07-14 16:52:29 +0330

    Fix special chars maximum length
    The range of random int for choosing number of puncuation chars included in password fixed to lengh of string.punctuaion in order to aviod including all list for most cases.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 letters = string.ascii_letters
 digits = string.digits
-special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, 50)]
+special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, len(string.punctuation))]
 
 pwd_len = input('Enter passwrod lenght: ')
 pwd_len = int(pwd_len) if pwd_len != '' else 6
