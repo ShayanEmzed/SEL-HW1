93a65431fad477a25bb635ec53426e8493e0d953
ShayanEmzed <shayan80sama@gmail.com>
2023-07-14 16:58:18 +0330

    Improve security
    The password requires to have at least one punctuation to considered to be strong.

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import random
 
 letters = string.ascii_letters
 digits = string.digits
-special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, 50)]
+special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, 20)]
 
 pwd_len = input('Enter passwrod lenght: ')
 pwd_len = int(pwd_len) if pwd_len != '' else 6
 alphabet = letters + digits + special_chars
+pwd_strong = False
 
 pwd = ''
-for _ in range(pwd_len):
-    pwd += random.choice(alphabet)
+while not pwd_strong:
+    for _ in range(pwd_len):
+        pwd += random.choice(alphabet)
+    
+    if any(c in special_chars for c in pwd):
+        pwd_strong = True
 
 print(pwd)
