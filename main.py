623a1876d4548fa24c38a78a5642024de58a5270
ShayanEmzed <shayan80sama@gmail.com>
2023-07-14 17:07:03 +0330

    Add details about final password
    Details like the number of letters and digits are displayed at the end.

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ letters = string.ascii_letters
 digits = string.digits
 special_chars = string.punctuation[:random.randint(len(string.punctuation) // 3, len(string.punctuation))]
 
-pwd_len = input('Enter passwrod lenght: ')
+pwd_len = input('Enter passwrod length: ')
 pwd_len = int(pwd_len) if pwd_len != '' else 6
 alphabet = letters + digits + special_chars
 pwd_strong = False
 
-pwd = ''
 while not pwd_strong:
+    pwd = ''
     for _ in range(pwd_len):
         pwd += random.choice(alphabet)
     
     if any(c in special_chars for c in pwd) and sum(c in digits for c in pwd) >= 2:
         pwd_strong = True
 
-print(pwd)
+print(f'Number of letters: {sum(c in letters for c in pwd)}')
+print(f'Number of digits: {sum(c in digits for c in pwd)}')
+print(f'Number of letters: {sum(c in special_chars for c in pwd)}')
+print()
+print(f'Password: {pwd}')
